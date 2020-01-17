9130dbea75d9c74f9ca98cbd81f0c80ecb508fe5
north <norther48@list.ru>
2020-01-17 15:45:22 +0300

    Add project files.

---
+++ flows/Data/FlowDbContext.cs
@@ -0,0 +1,16 @@
+﻿using flows.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace flows.Data
+{
+    public class FlowDbContext : DbContext
+    {
+        public FlowDbContext(DbContextOptions<FlowDbContext> options) : base(options) {}
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ flows/Data/Interfaces/IFlowContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace flows.Data.Interfaces
+{
+    public interface IFlowContextFactory
+    {
+        FlowDbContext CreateDbContext(string connectionString);
+    }
+}
