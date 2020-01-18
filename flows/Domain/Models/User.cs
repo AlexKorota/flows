7edb868967cc9fe17bd23155f1817b9fd0cafe37
north <norther48@list.ru>
2020-01-18 12:39:48 +0300

    user model modifications: - new fields - data annotations for validation

--- flows/Domain/Models/User.cs
+++ flows/Domain/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,23 @@ namespace flows.Domain.Models
 {
     public class User
     {
-        public int UserId { get; set; }
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 6)]
         public string Login { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 6)]
         public string Password { get; set; }
-        public bool isAdmin { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [StringLength(15, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        public string TelegramId { get; set; }
     }
 }
