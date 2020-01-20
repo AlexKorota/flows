d1703facbf05dd2a29cbe57cb3e555ad8c2f3293
north <norther48@list.ru>
2020-01-20 13:36:53 +0300

    add changeUserModel migration

---
+++ flows/Data/Migrations/20200120103549_ChangeUserModel.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace flows.Data.Migrations
+{
+    public partial class ChangeUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "isAdmin",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Login",
+                table: "Users",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                maxLength: 15,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TelegramId",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TelegramId",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Login",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isAdmin",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "UserId");
+        }
+    }
+}
