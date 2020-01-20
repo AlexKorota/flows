f99540291a2d720061b85062497a596c5fe91713
north <norther48@list.ru>
2020-01-20 14:03:28 +0300

    Add UserRepository with Interface

---
+++ flows/Domain/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using flows.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace flows.Domain.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task Create(User user);
+        Task Update(User user);
+
+        Task<User> GetById(int id);
+        Task<User> GetByEmail(string email);
+
+        Task<List<User>> GetList();
+    }
+}
---
+++ flows/Domain/Repositories/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using flows.Data;
+using flows.Domain.Models;
+using flows.Domain.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace flows.Domain.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly FlowDbContext _context;
+        public UserRepository(FlowDbContext context)
+        {
+            _context = context;
+        }
+        public Task Create(User user)
+        {
+            _context.Users.Add(user);
+            return _context.SaveChangesAsync();
+        }
+        public Task Update(User user)
+        {
+            _context.Users.Update(user);
+            return _context.SaveChangesAsync();
+        }
+
+        public Task<User> GetByEmail(string email)
+        {
+            return _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public Task<User> GetById(int id)
+        {
+            return _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public Task<List<User>> GetList()
+        {
+            return _context.Users.ToListAsync();
+        }
+    }
+}
