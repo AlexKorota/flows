16dd005f024b6f367488c64a55206df48e17303c
north <norther48@list.ru>
2020-02-03 15:07:38 +0300

    add BudgetDTO

--- flows/Domain/DTO/Budget/BudgetDTO.cs
+++ flows/Domain/DTO/Budget/BudgetDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using flows.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,9 @@ namespace flows.Domain.DTO.Budget
 {
     public class BudgetDTO
     {
-
+        public int Id;
+        public string Name;
+        public List<Expense> Expenses { get; set; }
+        public List<ExpensesGroup> ExpensesGroups { get; set; }
     }
 }
---
+++ flows/Domain/Services/BudgetService.cs
@@ -0,0 +1,41 @@
+﻿using flows.Domain.DTO.Budget;
+using flows.Domain.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace flows.Domain.Services
+{
+    public class BudgetService : IBudgetService
+    {
+        private readonly IBudgetRepository _budgetRepository;
+        public BudgetService()
+        {
+                
+        }
+        public Task<List<BudgetDTO>> GetAllUserBudgets(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BudgetDTO> GetUserBudget(int budgetId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<BudgetDTO> CreateUserBudget(BudgetDTO dto, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUserBudget(int budgetId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BudgetDTO> EditUserBudget(EditBudgetDTO dto, int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
